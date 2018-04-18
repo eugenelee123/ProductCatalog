1e4b790fd513d085b66373ac784c9027542f308b
marshall229 <36282437+marshall229@users.noreply.github.com>
2018-04-17 17:31:07 -0700

    Update Catalog.cpp

--- Catalog.cpp
+++ Catalog.cpp
@@ -38,7 +38,52 @@ int64_t	Catalog::GetProductCount(uint64_t categoryNumber)
 bool	Catalog::Load(const string& fileName)
 {
 	// to be completed
-	return true;
+	uint64_t categoryNumber,productNumber;
+	std::string categoryname, productname, line, tag;
+	std::fstream myfile(fileName);
+	number_of_products = 0;
+	number_of_categories = 0;
+
+
+	if (myfile.is_open())
+	{
+		while (std::getline(myfile, line))
+		{
+			std::istringstream ss(line);
+			size_t position = line.find("Category");
+			if (position != std::string::npos)
+			{
+				getline(ss, tag, '\t');
+				ss >> categoryNumber;
+				std::getline(ss, categoryname);
+				std::cout << "categoryNumber: " << categoryNumber << std::endl;
+				std::cout << "categoryname: " << categoryname << std::endl;
+				AddCategory(categoryNumber, categoryname);
+				number_of_categories++;
+			}
+
+			else if (position == std::string::npos)
+			{
+				ss >> productNumber;
+				getline(ss, productname);
+				std::cout << "categoryNumber: " << categoryNumber << std::endl;
+				std::cout << "productNumber: " << productNumber << std::endl;
+				std::cout << "productname: " << productname << std::endl;
+				AddProduct(categoryNumber, productNumber, productname);
+				number_of_products++;
+			}
+
+
+		}
+		myfile.close();
+		return true;
+	}
+
+	else
+	{
+		throw std::invalid_argument("Could not open file" + fileName);
+		return false;
+	}
 }
 
 //****************************************************************************************
